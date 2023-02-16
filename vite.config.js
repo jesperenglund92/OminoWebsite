f9ddb525061345d41ae6d0bd7679254d2d9d53e2
jesperenglund92 <49065682+jesperenglund92@users.noreply.github.com>
2023-02-16 14:42:51 +0100

    vite config update 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://jesperenglund92.github.io/OminoWebsite',
+  base: '/OminoWebsite/',
   plugins: [react()],
 })
