13944ae9ff5483bb18926dce772fcfcdde3d53bb
jesperenglund92 <49065682+jesperenglund92@users.noreply.github.com>
2023-05-21 16:07:32 +0200

    copy update

--- src/features/Us.jsx
+++ src/features/Us.jsx
@@ -7,8 +7,8 @@ const Us = () => {
                 <div className="new-grid us-grid">
                     <div className="multi-paragraph-item us-text">
                         <p>It all started as a school project in the fall of 2020 at KTH in Stockholm. Since the project was complete, the development has continued intermitttently and now it's finally starting to come together.</p>
-                        <p>Hannes Runelöv had the original idea for the core mechanics of the game, such as rolling and attaching to other cubes. He also developed these mechanics in addition to some environment interactions, level design, setting up the initial project structure, Omino creature animations, as well as shaders, such as ice and conveyor belts.</p>
-                        <p>Everything else that you see and hear in the game was designed and developed by Jesper Englund, including game logic, level editor, data architecture, UI, movement mechanics and environment interactions, level design, music, sound effects, skyboxes and this website. Jesper is the only one still working on the project.</p>
+                        <p>Hannes Runelöv has been a crucial cog in this project, as he had the idea for the core mechanics of the game. He also developed these mechanics in addition to Omino creature animations, as well as creating shaders, such as ice and conveyor belts. Due to personal reasons, he will just stay on as a level designer and tester going forward.</p>
+                        <p>Everything else that you see and hear in the game was designed and developed by Jesper Englund, including game logic, level editor, data architecture, UI, movement mechanics and environment interactions, level design, music, sound effects, skyboxes and this website.</p>
                     </div>
                     <div className="center">
                         <div className="us-img-container">
