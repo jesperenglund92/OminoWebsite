d05c57a4e36bfa8745f26443e60ac12322239ed4
jesperenglund92 <49065682+jesperenglund92@users.noreply.github.com>
2024-01-05 13:07:55 +0100

    tiktok link update

--- src/features/Components/Splash.jsx
+++ src/features/Components/Splash.jsx
@@ -6,7 +6,7 @@ import { ICONS } from "../../utils/utils";
 const Splash = () => {
     const icons = [{ name: 'fa-facebook', link: 'https://www.facebook.com/profile.php?id=100090126797341' },
     { name: 'fa-instagram', link: 'https://www.instagram.com/ominogames/' },
-    { name: 'fa-tiktok', link: 'https://www.tiktok.com/@omino.games' },
+    { name: 'fa-tiktok', link: 'https://www.tiktok.com/@ominogames' },
     { name: 'fa-youtube', link: 'https://www.youtube.com/@OminoGames' },
     { name: 'fa-kickstarter-k', link: 'https://www.kickstarter.com/projects/bockpe/polipod-a-fun-mobile-puzzle-game' },
     { name: 'fa-discord', link: 'https://discord.com/channels/1074707561833562145/1074707563301572619' }];
