2639ff28afa1671a8b841c82da266a41ecabc2bd
jesperenglund92 <49065682+jesperenglund92@users.noreply.github.com>
2024-03-16 17:53:58 +0100

    test hash routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from './HomePage';
 import PrivacyPolicy from './PrivacyPolicy';
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
-          <Route index element={<HomePage />} />
-          <Route path="polipod-privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/polipod-privacy-policy" element={<PrivacyPolicy />} />
       </Routes>
-    </BrowserRouter>
   );
 }
 
